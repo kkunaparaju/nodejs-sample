b54aa0d94c99f0412fd1cfd498eeb61f729a8b87
karthick kunaparaju <kkunaparaju@expedia.com>
2015-10-22 11:09:37 -0700

    hello added

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+app.set('port', process.env.PORT || 3000);
+var os = require('os');
+
+app.get('/test', function(req, res) {
+  for(var i = 0; i < 10000; i ++){
+    console.log(i);
+  }
+  res.send('Hello World!');
+});
+
+app.get('/', function(req, res){
+	res.send(os.hostname());
+});
+
+app.listen(app.get('port'));
