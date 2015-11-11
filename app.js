df39fbe5b4a7ce5cef3cdab4b830a859d6d1dd29
karthick kunaparaju <kkunaparaju@expedia.com>
2015-11-11 14:29:38 -0800

    just s

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/test', function(req, res) {
 });
 
 app.get('/', function(req, res){
-	res.send("Server: " +os.hostname());
+	res.send("S: " +os.hostname());
 });
 
 app.listen(app.get('port'));
